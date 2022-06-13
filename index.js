18e2370ec7a30ec5d3d7362548947da4cde6db62
Kofi oghenevwegba <106738874+Kofi-js@users.noreply.github.com>
2022-06-13 22:07:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let male = ['ogaga', 'bayo', 'kola', 'kofi'];
+let female = ['bola', 'glory', 'joy', 'ofe'];
+let place = ['sketch', 'Trenches', 'dominos', 'dubai']
+
+
+let rMale = male[Math.floor(Math.random() * male.length)]
+let rFemale = female[Math.floor(Math.random() * male.length)]
+let rPlace = place[Math.floor(Math.random() * male.length)]
+
+
+
+console.log(`${rMale} is taking ${rFemale} to ${rPlace}`)
